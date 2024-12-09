f53022fde4ca50277671dd90a76ec5aca839bd34
JeongRoh Yoon <110525482+JeongNoh-Yoon@users.noreply.github.com>
2024-12-09 12:56:31 +0900

    Update utils.py

--- Project/utils.py
+++ Project/utils.py
@@ -1,1 +1,14 @@
+import torch
+import pandas as pd
+
+def load_model(model_path, input_dim):
+    """Load the trained model."""
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+    return model
+
+def load_sample_data(data_path):
+    """Load sample data from DataFrame."""
+    df = pd.read_csv(data_path)
+    return df
 
